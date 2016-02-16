33182486f9aff234ef5a6edec69fc6657ed8bcb5
Lewis <lewis.haris@gmail.com>
2016-02-16 13:13:42 +0800

    Add given sound sampling code

---
+++ app/src/main/java/sg/edu/nus/cs3218tut_lewisharisnata/SoundSamplingActivity.java
@@ -0,0 +1,63 @@
+package com.example.ngtk.cs3218alltutorials;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class SoundSamplingActivity extends Activity {
+
+
+    public  static          CSurfaceViewSoundSampling   	surfaceView;
+    private SoundSampler   	soundSampler;
+    public  static short[]  buffer;
+    public  static int      bufferSize;     // in bytes
+
+    public void goToMainActivity(View view){
+
+        // --- fill up codes here to end all drawings and sound sampling before returning to MainActivity
+
+        /****/
+        
+        /****/
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_sampling);
+
+        try {
+            soundSampler = new SoundSampler(this);
+
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Cannot instantiate SoundSampler", Toast.LENGTH_LONG).show();
+        }
+
+        try {
+            soundSampler.init();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(),"Cannot initialize SoundSampler.", Toast.LENGTH_LONG).show();
+        }
+
+        surfaceView = (CSurfaceViewSoundSampling)findViewById(R.id.surfaceView);
+        surfaceView.drawThread.setBuffer(buffer);
+    }
+
+
+    public void captureSound(View v) {
+        if (surfaceView.drawThread.soundCapture) {
+            surfaceView.drawThread.soundCapture = Boolean.valueOf(false);
+            surfaceView.drawThread.segmentIndex = -1;
+        }
+        else {
+            surfaceView.drawThread.soundCapture = Boolean.valueOf(true);
+
+        }
+    }
+
+
+
+}
