b7d8611273fae831856de38dd9deefcb17200d54
Lewis <lewis.haris@gmail.com>
2016-03-01 16:17:27 +0800

    Add calculus activity

--- app/src/main/java/sg/edu/nus/cs3218tut_lewisharisnata/MainActivity.java
+++ app/src/main/java/sg/edu/nus/cs3218tut_lewisharisnata/MainActivity.java
@@ -3,6 +3,8 @@ package sg.edu.nus.cs3218tut_lewisharisnata;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void goToSound(View view) {
         Intent intent = new Intent(this, SoundSamplingActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToCalculus(View view) {
+        Intent intent = new Intent(this, CalculusActivity.class);
         startActivity(intent);
     }
 }
